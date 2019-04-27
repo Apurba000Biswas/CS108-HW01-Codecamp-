6d76fbba654faa76db9e76b20f291fcc1413d49a
apurba <apurba10024@gmail.com>
2019-04-27 18:26:54 +0600

    1.1 Added Solution to String Code blowup() method

--- StringCode.java
+++ StringCode.java
@@ -1,4 +1,6 @@
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 // CS108 HW1 -- String static methods
@@ -14,7 +16,10 @@ public class StringCode {
 	public static int maxRun(String str) {
 		return 0; // YOUR CODE HERE
 	}
-
+	
+	public static void main(String[] args) {
+		blowup("");
+	}
 	
 	/**
 	 * Given a string, for each digit in the original string,
@@ -24,7 +29,37 @@ public class StringCode {
 	 * @return blown up string
 	 */
 	public static String blowup(String str) {
-		return null; // YOUR CODE HERE
+		Queue<Character> frequencyQueue = new LinkedList<Character>();
+		for(int i=0; i<str.length(); i++) {
+			char ch = str.charAt(i);
+			if(Character.isDigit(ch)) {
+				if(i < str.length()-1) {
+					int frequency = Character.getNumericValue(ch);
+					char character = str.charAt(i+1);
+					putInFrquencyQueue(frequency, character, frequencyQueue);
+				}
+			}
+		}
+		String result = getString(str, frequencyQueue);
+		return result;
+	}
+	
+	private static void putInFrquencyQueue(int freq, char character, Queue<Character> queue) {
+		for(int i=0; i<freq; i++) {
+			queue.add(character);
+		}
+	}
+	
+	private static String getString(String originalString, Queue<Character> freqQueue) {
+		StringBuilder sb = new StringBuilder("");
+		for(int i=0; i<originalString.length(); i++) {
+			while(!freqQueue.isEmpty() && originalString.charAt(i) == freqQueue.peek()) {
+				sb.append(freqQueue.remove());
+			}
+			char ch = originalString.charAt(i);
+			if(!Character.isDigit(ch))sb.append(originalString.charAt(i));
+		}
+		return sb.toString();
 	}
 	
 	/**
